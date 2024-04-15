abe34d9dcc6b8add088baabb24cd45f14a1e0b63
AkashkumarChoudhary <akcmumbai13579@gmail.com>
2024-04-15 23:45:35 +0530

    15

--- 05_PasswordGenerator/src/App.jsx
+++ 05_PasswordGenerator/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useCallback } from 'react'
  
 import './App.css'
 
@@ -6,9 +6,22 @@ function App() {
   const [length , setLength] = useState(8)
   const [numberAllowed, setnumberAllowed] = useState(false)
   const[charAllowed , setcharAllowed] = useState(false)
-  const[password, setPassword] = useState("")
+  const[password, setPassword ] = useState("") //  password is variable , setpassword is method
   
+  const passwordGenerator = useCallback(() =>{
+    let pass = "" 
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    if(numberAllowed) str += "0123456789"
+    if(charAllowed) str += "!@#$%^&*()-_+={}~` "
 
+    for (let i = 0; i <= array.length; i++) {
+       let char = Math.floor(Math.random() * str.lenght +1)
+      
+    }
+  },[ length , numberAllowed , charAllowed, setPassword])
+  
+  
+  
   return (
     <>
       <h1  className='text-4xl text-center
